9f5120dbf65a283a5aa39d04f200eeded55f3561
hy040504 <hy040504@users.noreply.github.com>
2026-09-07 12:21:33 +0900

    fix: 모듈 설치 패키징 설정 최신화 (setup.py)
    - install_requires에 requests 의존성 명시
    - repository URL 및 long_description markdown 설정 적용
    - Python 3.x 분류기 업데이트

--- setup.py
+++ setup.py
@@ -6,22 +6,35 @@ from setuptools import find_packages, setup
 
 
 def long_description():
-    with io.open('README.rst', 'r', encoding='utf-8') as f:
+    with io.open('README.md', 'r', encoding='utf-8') as f:
         readme = f.read()
     return readme
 
-setup(name='papago',
-      version='2.0.0',
-      description='PAPAGO translate API with Python',
-      long_description=long_description(),
-      url='https://github.com/stunstunstun/papago',
-      author='stunstunstun',
-      author_email='agileboys.com@gmail.com',
-      license='MIT',
-      packages=find_packages(),
-      classifiers=[
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.6'],
-      zip_safe=False,
-      test_suite='tests')
+
+setup(
+    name='papago',
+    version='2.0.0',
+    description='Unofficial Naver Papago Translate API for Python',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/hy040504/papago',
+    author='hy040504',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.20.0',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    zip_safe=False,
+    test_suite='tests',
+)
